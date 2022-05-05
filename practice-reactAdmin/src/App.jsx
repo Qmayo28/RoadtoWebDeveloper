f231258865e6db4f43a54f375563644687b45cb9
Qmayo28 <104951746+Qmayo28@users.noreply.github.com>
2022-05-05 10:04:57 +0800

    Add files via upload

---
+++ practice-reactAdmin/src/App.jsx
@@ -0,0 +1,45 @@
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import './app.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUSer';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+        </Routes>
+        <Routes>
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+        <Routes>
+          <Route path="/user/:userId" element={<User />} />
+        </Routes>
+        <Routes>
+          <Route path="/NewUser" element={<NewUser />} />
+        </Routes>
+        <Routes>
+          <Route path="/products" element={<ProductList />} />
+        </Routes>
+        <Routes>
+          <Route path="/product/:productId" element={<Product />} />
+        </Routes>
+        <Routes>
+          <Route path="/newproduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
