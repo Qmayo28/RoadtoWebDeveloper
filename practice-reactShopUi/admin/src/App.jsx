fa08fdb8bb4e6cca19fb1b51ccaab0507a951df0
Qmayo28 <104951746+Qmayo28@users.noreply.github.com>
2022-05-05 10:10:20 +0800

    Add files via upload

---
+++ practice-reactShopUi/admin/src/App.jsx
@@ -0,0 +1,55 @@
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import './app.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUSer';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
+import Login from './pages/login/Login';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {admin && (
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+            </Routes>
+            <Routes>
+              <Route path="/users" element={<UserList />} />
+            </Routes>
+            <Routes>
+              <Route path="/user/:userId" element={<User />} />
+            </Routes>
+            <Routes>
+              <Route path="/NewUser" element={<NewUser />} />
+            </Routes>
+            <Routes>
+              <Route path="/products" element={<ProductList />} />
+            </Routes>
+            <Routes>
+              <Route path="/product/:productId" element={<Product />} />
+            </Routes>
+            <Routes>
+              <Route path="/newproduct" element={<NewProduct />} />
+            </Routes>
+          </div>
+        </>
+      )}
+    </Router>
+  );
+}
+
+export default App;
---
+++ practice-reactShopUi/admin/src/redux/store.js
@@ -0,0 +1,82 @@
+// // import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore, combineReducers } from '@reduxjs/toolkit';
+// import userReducer from './userRedux';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// const persistConfig = {
+//   key: 'root',
+//   version: 1,
+//   storage,
+// };
+
+// //Persist will stay the cart counter and login active
+
+// const rootReducer = combineReducers({ user: userReducer });
+// // const persistedReducer = persistReducer(persistConfig, userReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+// export const store = configureStore({
+//   // reducer: {
+//   //   cart: cartReducer,
+//   //   // user: userReducer,
+//   //   user: persistedReducer,
+//   // },
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export let persistor = persistStore(store);
+
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './userRedux';
+import productReducer from './productRedux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  product: productReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
